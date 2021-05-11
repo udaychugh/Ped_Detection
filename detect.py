c30690735090f976c4e1b1256dff5ec782f66096
Uday Chugh <61309187+udaychugh@users.noreply.github.com>
2021-05-11 11:23:04 +0530

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,28 @@
+import cv2
+
+print("Project Name = Vechile Classification")
+
+video_src = input("Enter video source=")
+
+cap = cv2.VideoCapture(video_src)
+
+peds_cascade = cv2.CascadeClassifier('pedestrian.xml')
+
+while True:
+	ret, img = cap.read()
+
+	if(type(img) == type(None)):
+		break
+
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	peds = peds_cascade.detectMultiScale(gray, 1.3, 2)
+
+	for(a,b,c,d) in peds:
+		cv2.rectangle(img, (a,b), (a+c, b+d), (0, 255, 210), 4)
+
+	cv2.imshow('video', img)
+
+	if cv2.waitkey(33) == 27:
+		break
+
+cv2.destroyAllWindows() 
